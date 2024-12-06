d7b5476a372501d44129b01f81ffec9953813fa2
php8database <php8database@gmail.com>
2024-12-06 16:32:59 +0800

    刪除用不到的後端API

--- api/db.php
+++ api/db.php
@@ -1,7 +1,7 @@
 <?php
 
 class DB{
-    protected $dsn="mysql:host=localhost;charset=utf8;dbname=db07";
+    protected $dsn="mysql:host=localhost;charset=utf8;dbname=db10";
     protected $pdo;
     protected $table;
 
@@ -145,7 +145,7 @@ class DB{
 }
 
 function q($sql){
-    $pdo=new PDO("mysql:host=localhost;charset=utf8;dbname=db07",'root','');
+    $pdo=new PDO("mysql:host=localhost;charset=utf8;dbname=db10",'root','');
     return $pdo->query($sql)->fetchAll();
 }
 
@@ -162,3 +162,5 @@ function to($url){
 
 $Title=new DB('titles');
 $Ad=new DB('ads');
+$Image=new DB('images');
+$Mvim=new DB('mvims');

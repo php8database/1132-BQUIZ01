22339706107282a0c8c8b86d130fc4c11cdfb532
php8database <php8database@gmail.com>
2024-12-13 13:50:52 +0800

    在標題圖片加上替代文字顯示

--- api/db.php
+++ api/db.php
@@ -1,7 +1,7 @@
 <?php
 
 class DB{
-    protected $dsn="mysql:host=localhost;charset=utf8;dbname=db10";
+    protected $dsn="mysql:host=localhost;charset=utf8;dbname=db07";
     protected $pdo;
     protected $table;
 
@@ -145,7 +145,7 @@ class DB{
 }
 
 function q($sql){
-    $pdo=new PDO("mysql:host=localhost;charset=utf8;dbname=db10",'root','');
+    $pdo=new PDO("mysql:host=localhost;charset=utf8;dbname=db07",'root','');
     return $pdo->query($sql)->fetchAll();
 }
 
